ba6bf5d2e7ef5cd86010011459067b3b3434180e
TonySauceda <asauceda@boz.mx>
2021-10-01 15:43:15 -0500

    Custom hook y componente input

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import useForm from './hooks/useForm'
+import Input from './components/Input'
+
 function App() {
-	return <div>Gestor de usuarios</div>
+	const [form, handleChange] = useForm({ name: '', apellido: '' })
+
+	console.log(form)
+	return (
+		<form>
+			<Input label="Nombre: " name="name" placeholder="Nombre" value={form.name} onChange={handleChange} />
+			<Input label="Apellido: " name="lastname" placeholder="Apellido" value={form.name} onChange={handleChange} />
+		</form>
+	)
 }
 
 export default App
